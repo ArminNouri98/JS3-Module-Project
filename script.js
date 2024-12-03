606bbc3e73d6282beb6826c9528d5b861731c111
ArminNouri <nouriarmin24@gmail.com>
2024-12-03 18:19:16 +0000

    Write function makePageForEpisodes for show all of the series

--- script.js
+++ script.js
@@ -6,7 +6,26 @@ function setup() {
 
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
-}
 
+  episodeList.forEach((episode) => {
+    const card = document.createElement("div");
+    card.className = "card";
+    card.innerHTML = `
+  
+      <img src="${episode.image.medium}" alt="${episode.name}">
+      <div class="card-content">
+        <h3>${episode.name} - S${String(episode.season).padStart(
+      2,
+      "0"
+    )}E${String(episode.number).padStart(2, "0")}</h3>
+        <p>${episode.summary}</p>
+        <a href="${episode.url}" target="_blank">More info</a>
+      </div>
+    
+
+    `;
+
+    rootElem.appendChild(card);
+  });
+}
 window.onload = setup;
